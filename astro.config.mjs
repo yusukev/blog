d56cf78402983cb34532edc22fc44a9e1c957728
yusukev <hi@yusukev.com>
2026-03-25 06:52:36 +0900

    Deploy to Cloudflare Workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://yusukev.com',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
 });
