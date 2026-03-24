f403576ad10e660364b162e0d06e9f8032891785
yusukev <hi@yusukev.com>
2026-03-25 06:59:31 +0900

    start using prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yusukev.com',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
-});
+})
