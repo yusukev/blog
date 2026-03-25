34ba8a417ae33d1b29e4d55b3443b97db0cbfe3e
yusukev <hi@yusukev.com>
2026-03-25 21:41:47 +0900

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Yusuke\'s blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'Getting to nowork'
+export const SITE_DESCRIPTION = 'Let\'s get to nowork.'
