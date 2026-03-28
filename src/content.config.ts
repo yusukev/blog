0f4149504eaa0a9f1ccb84761858f25933b5ed9c
yusukev <hi@yusukev.com>
2026-03-28 10:36:36 +0900

    reorganize

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,8 +3,8 @@ import { glob } from 'astro/loaders'
 import { z } from 'astro/zod'
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  // Load Markdown and MDX files in the `src/content/` directory.
+  loader: glob({ base: './src/content', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
